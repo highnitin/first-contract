8cd6ba355c9a9b6dad1fabaef2304ffac6fdbca3
highnitin <nitinjoshi983@gmail.com>
2024-01-06 20:39:42 +0530

    Create working first contract

---
+++ Hello-World/hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+};
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/PvNpbR-hUb5T0cXvJSaY7QRwNO89dIdx",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
+
+
